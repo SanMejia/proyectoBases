92698726abd256e05750e8909eca80ce81c19948
JuanDavidDelgado <juan.rubiano@correounivalle.edu.co>
2020-05-22 17:32:46 -0500

    Modificando conexiones

--- Frontend/src/components/RegistroTrabajador.js
+++ Frontend/src/components/RegistroTrabajador.js
@@ -1,7 +1,42 @@
 import React,{Component} from 'react';
+import axios from 'axios'
 
 class RegistroTrabajador extends Component{
 
+
+    state = {
+        nombreTrabajador: '',
+        password: '',
+        telefono: '',
+        direccion: '',
+        email: '',
+        noTarjeta: ''
+    }
+
+    onChangeNombre = (e) => {
+        this.setState({nombreTrabajador: e.target.value})
+       }
+       onChangeContraseña = (e) => {
+         this.setState({password: e.target.value})
+       }
+   
+       onChangeDireccion = (e) => {
+         this.setState({direccion: e.target.value})
+       }
+   
+       onChangeTelefono = (e) => {
+         this.setState({telefono: e.target.value})
+       }
+   
+       onChangeEmail = (e) => {
+         this.setState({email: e.target.value})
+       }
+   
+       onChangeTarjeta = (e) => {
+         this.setState({noTarjeta: e.target.value})
+       }
+
+     
     render(){
 
         return(
@@ -12,22 +47,22 @@ class RegistroTrabajador extends Component{
             <form>
                 {/*<!-- NOMBRE -->*/}
             <label for="Nombre">Nombre:</label>
-                <input type="text" placeholder="Ingrese Nombre de Usuario" />
+                <input type="text" placeholder="Ingrese Nombre de Usuario" onChange={this.onChangeNombre} />
                 {/*<!-- CONTRASEÑA -->*/}
             <label for="Contraseña">Contraseña:</label>
-                <input type="text" placeholder="Ingrese Contraseña" />
+                <input type="text" placeholder="Ingrese Contraseña"  onChange={this.onChangeContraseña}/>
                 {/*<!-- TELEFONO -->*/}
             <label for="Telefono">Telefono:</label>
-                <input type="text" placeholder="Ingrese Telefono" />
+                <input type="text" placeholder="Ingrese Telefono" onChange={this.onChangeTelefono} />
                 {/*<!-- DIRECCION -->*/}
             <label for="Direccion">Direccion:</label>
-                <input type="text" placeholder="Ingrese Direccion" />
+                <input type="text" placeholder="Ingrese Direccion" onChange={this.onChangeDireccion} />
                 {/*<!-- EMAIL -->*/}
             <label for="Email">Email:</label>
-                <input type="text" placeholder="Ingrese Email" />
+                <input type="text" placeholder="Ingrese Email" onChange={this.onChangeEmail} />
                 {/*<!-- TARJETA -->*/}
             <label for="NoTarjeta">NoTarjeta:</label>
-                <input type="text" placeholder="Ingrese No Tarjeta" />
+                <input type="text" placeholder="Ingrese No Tarjeta" onChange={this.onChangeTarjeta} />
                 {/*<!-- DOCUMENTO IDENTIDAD -->*/}
             <label for="Documento">Documento identidad:</label>
                 <a href="#">Ingresar Documento</a><br />
@@ -36,7 +71,7 @@ class RegistroTrabajador extends Component{
                 <a href="#">Ingresar Fotografia</a><br />
 
         
-            <input class="Registrarme" type="submit" value="Registrarme" />
+            <input class="Registrarme" type="submit" value="Registrarme"  />
             <input class="Cancelar" type="submit" value="Cancelar" />
             <a href="#">Ayuda??</a><br />
         </form>
--- Frontend/src/components/RegistroUsuario.js
+++ Frontend/src/components/RegistroUsuario.js
@@ -1,7 +1,40 @@
 import React,{Component} from 'react';
 import TipoRegistro from './TipoRegistro';
+import axios from 'axios'
 
 class RegistroUsuario extends Component{
+
+    state = {
+     nombreUsuario: '',
+     password: '',
+     telefono: '',
+     direccion: '',
+     email: '',
+     noTarjeta: ''
+    }
+
+    onChangeUsuario = (e) => {
+     this.setState({nombreUsuario: e.target.value})
+    }
+    onChangeContraseña = (e) => {
+      this.setState({password: e.target.value})
+    }
+
+    onChangeDireccion = (e) => {
+      this.setState({direccion: e.target.value})
+    }
+
+    onChangeTelefono = (e) => {
+      this.setState({telefono: e.target.value})
+    }
+
+    onChangeEmail = (e) => {
+      this.setState({email: e.target.value})
+    }
+
+    onChangeTarjeta = (e) => {
+      this.setState({noTarjeta: e.target.value})
+    }
 
     render(){
 
@@ -13,22 +46,22 @@ class RegistroUsuario extends Component{
             <form>
               {/*<!-- NOMBRE -->*/}
               <label for="Nombre">Nombre:</label>
-              <input type="text" placeholder="Ingrese Nombre de Usuario" />
+              <input type="text" placeholder="Ingrese Nombre de Usuario" onChange={this.onChangeUsuario} />
                {/*<!-- CONTRASEÑA -->*/}
               <label for="Contraseña">Contraseña:</label>
-              <input type="text" placeholder="Ingrese Contraseña" />
+              <input type="text" placeholder="Ingrese Contraseña" onChange={this.onChangeContraseña}/>
                {/*<!-- TELEFONO -->*/}
               <label for="Telefono">Telefono:</label>
-              <input type="text" placeholder="Ingrese Telefono" />
+              <input type="text" placeholder="Ingrese Telefono" onChange={this.onChangeTelefono}/>
                {/*<!-- DIRECCION -->*/}
               <label for="Direccion">Direccion:</label>
-              <input type="text" placeholder="Ingrese Direccion" />
+              <input type="text" placeholder="Ingrese Direccion" onChange={this.onChangeDireccion}/>
                {/*<!-- EMAIL -->*/}
               <label for="Email">Email:</label>
-              <input type="text" placeholder="Ingrese Email" /> 
+              <input type="text" placeholder="Ingrese Email" onChange={this.onChangeEmail}/> 
                {/*<!-- TARJETA -->*/}
               <label for="NoTarjeta">NoTarjeta:</label>
-              <input type="text" placeholder="Ingrese No Tarjeta" />
+              <input type="text" placeholder="Ingrese No Tarjeta"onChange={this.onChangeTarjeta} />
       
                {/*<!-- SERVICIOS -->*/}
               <label for="Recibo Servicios">Recibo Servicios:</label>
